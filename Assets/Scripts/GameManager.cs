3da32091628aba552c440d74372bf4da5b832957
Ayşe Oduncu <59964531+codergirll-droid@users.noreply.github.com>
2023-01-20 17:46:14 +0300

    Updates
    Added physics materials
    Added some controls

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,23 +7,72 @@ public class GameManager : MonoBehaviour
     public List<int> boxNumbers;
     public List<Material> boxMaterials;
 
+    public GameObject box;
+    public float touchAmountRatio;
+
     private void Start()
     {
         boxNumbers = new List<int> { 2, 4, 8, 16, 32, 64, 128, 512, 1024, 2048 };
 
     }
 
-    void boxSpawner()
+    private void Update()
+    {
+        Controls();
+    }
+
+    void Controls()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = new Vector2();
+            float touchAmount = 0;
+            if(touch.phase == TouchPhase.Began)
+            {
+                touchPos = touch.deltaPosition;
+            }
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                if((touchPos.x - touch.deltaPosition.x) > 0)
+                {
+                    touchAmount = -touchAmountRatio;
+
+                }else if((touchPos.x - touch.deltaPosition.x) < 0)
+                {
+                    touchAmount = touchAmountRatio;
+                }
+                else
+                {
+                    touchAmount = 0;
+                }
+
+                box.transform.position += new Vector3(touchAmount, 0, 0);
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                box.GetComponent<Rigidbody>().AddForce(Vector3.forward * 1000);
+            }
+
+
+
+
+        }
+    }
+
+    void BoxSpawner()
     {
 
     }
 
-    public void boxUpdater()
+    public void BoxUpdater()
     {
 
     }
 
-    public void boxDestroyer()
+    public void BoxDestroyer()
     {
 
     }
